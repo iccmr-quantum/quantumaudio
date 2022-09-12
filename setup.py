b4348f95812dba754dbd841f14ef51eb4ccac329
Itaborala <itaborapuy@gmail.com>
2022-09-12 12:41:20 +0100

    Create basic packaging structure

---
+++ setup.py
@@ -0,0 +1,3 @@
+import setuptools
+
+setuptools.setup(name='quantumaudio', version='0.1.2', description = 'A python module for building circuits for quantum audio encoding/decoding.', author='Itaborala', install_requires=['qiskit', 'numpy', 'ipython', 'matplotlib', 'bitstring'], author_email='', packages=['quantumaudio'], zip_safe=False)
