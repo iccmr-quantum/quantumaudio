73dc8e4d91336528496c50c19092094a08cfae4d
Itaborala <itaborapuy@gmail.com>
2022-12-23 02:33:10 +0000

    setup.py -> pyproject.toml

--- quantumaudio/__init__.py
+++ quantumaudio/__init__.py
@@ -1,1 +1,8 @@
-from .quantumaudio import QuantumAudio, QPAM, SQPAM, QSM
+"""quantumaudio: A Python class implementation for Quantum Representations of Audio in Qiskit.
+
+Itaborala @ QuTune https://github.com/iccmr-quantum/quantumaudio
+"""
+
+__version__ = '0.0.2'
+
+from .quantumaudio import QuantumAudio, QPAM, SQPAM, QSM, EncodingScheme
