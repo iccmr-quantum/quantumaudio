b1807a7dc8236c209cc62c817fce8bcb56af9f2c
Itaborala <itaborapuy@gmail.com>
2022-12-23 03:56:19 +0000

    creating new package with pyproject.toml

--- quantumaudio/__init__.py
+++ quantumaudio/__init__.py
@@ -1,7 +1,4 @@
-"""quantumaudio: A Python class implementation for Quantum Representations of Audio in Qiskit.
-
-Itaborala @ QuTune https://github.com/iccmr-quantum/quantumaudio
-"""
+"""quantumaudio: A Python class implementation for Quantum Representations of Audio in Qiskit."""
 
 __version__ = '0.0.2'
 
