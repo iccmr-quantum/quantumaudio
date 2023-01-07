436c12730dac10326975327190521a0cae4539e4
itaborala <28213905+Itaborala@users.noreply.github.com>
2023-01-07 16:18:56 +0000

    bump version to official release

--- quantumaudio/__init__.py
+++ quantumaudio/__init__.py
@@ -1,5 +1,5 @@
 """quantumaudio: A Python class implementation for Quantum Representations of Audio in Qiskit. Developed by the quantum computer music team at the Interdisciplinary Centre for Computer Music Research, University of Plymouth, UK"""
 
-__version__ = '0.0.2rc2'
+__version__ = '0.0.2'
 
 from .quantumaudio import QuantumAudio, QPAM, SQPAM, QSM, EncodingScheme
